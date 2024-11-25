290a6a69d1a404640b62b011505a59243473525f
Nikolay Popov <helioproduct.sync@gmail.com>
2024-11-25 22:32:44 +0300

    migrations and deployments init

--- cmd/musicapp/main.go
+++ cmd/musicapp/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"music-service/internal/config"
 	"music-service/pkg/logger"
+
+	"github.com/golang-migrate/migrate"
 )
 
 func main() {
@@ -14,5 +17,19 @@ func main() {
 	}
 	logger := logger.New(cfg.Env)
 	logger.Info("config read")
+
+	m, err := migrate.New("file://"+cfg.Postgres.Migrations, cfg.Postgres.URI)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	if err := m.Up(); err != nil {
+		// already up
+		if errors.Is(err, migrate.ErrNoChange) {
+			logger.Info("no migrations to apply")
+		} else {
+			logger.Fatal(err.Error())
+		}
+	}
 
 }
