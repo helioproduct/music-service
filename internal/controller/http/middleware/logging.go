b06906c28886775a7a792be23d0f62942eb27b77
Nikolay Popov <helioproduct.sync@gmail.com>
2024-11-26 15:27:44 +0300

    recover and logging middleware added

--- internal/controller/http/middleware/logging.go
+++ internal/controller/http/middleware/logging.go
@@ -1,1 +1,24 @@
 package middleware
+
+import (
+	"music-service/pkg/logger"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func Logging(logger logger.Logger) mux.MiddlewareFunc {
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			logger.Info("Started request", "method", r.Method, "url", r.URL.Path)
+
+			next.ServeHTTP(w, r)
+
+			duration := time.Since(start)
+			logger.Info("Completed request", "method", r.Method, "url", r.URL.Path, "duration", duration)
+		})
+	}
+}
--- internal/controller/http/middleware/recover.go
+++ internal/controller/http/middleware/recover.go
@@ -1,1 +1,26 @@
 package middleware
+
+import (
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/gorilla/mux"
+)
+
+func Panic(logger *slog.Logger) mux.MiddlewareFunc {
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					logger.Error("recovere middleware", "error", err)
+					debug.PrintStack()
+					// logger.Error("panic", "stack", )
+					http.Error(w, "Internal server error", 500)
+				}
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
